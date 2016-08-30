4f0a0e911c70bc19735209c7a032b04b0ec4dbf2
Kirk deDoes <tea@dedogs.com>
2016-08-30 03:43:38 -0400

    Inital

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using deDogs.Toolbox.Image;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace com.PhotoSamples.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            List<string> imageProperties = new List<string>();
+
+            DirectoryInfo directory = new DirectoryInfo(Server.MapPath("~/Images"));
+
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                ImageProperties imageProperty = new ImageProperties
+                {
+                    Name = file.Name,
+                    Path = "/Images",
+                    Dimensions = new Dimension<int>
+                    {
+                        Width = 300,
+                        Height = 300
+                    }
+                };
+
+                imageProperties.Add("/thumbImage?p=" + serializer.Serialize(imageProperty));
+            }
+            return View(imageProperties);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/ThumbHandler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Drawing;
+using deDogs.Toolbox;
+using deDogs.Toolbox.Image;
+using System.Web.Routing;
+using System.IO;
+using System.Collections.Specialized;
+namespace com.PhotoSamples.Models
+{
+    public class ThumbHandler : IRouteHandler, IDisposable
+    {
+
+        //Expected JSON string: p = {dimensions: { int, int } , name: string, path: string, title: string, type: string}
+        protected Bitmap bitMap;
+
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            if (_disposed)
+                throw new ObjectDisposedException("ThumbHandlerState");
+
+            NameValueCollection routeValues = requestContext.HttpContext.Request.QueryString;
+            if (!String.IsNullOrEmpty(routeValues.Get("p")))
+            {
+                var serializer = new JavaScriptSerializer();
+
+                ImageProperties imageProperties = serializer.Deserialize<ImageProperties>(routeValues.Get("p"));
+
+                string path = "~" + imageProperties.Path + "/" + imageProperties.Name + imageProperties.Type;
+
+                using (Thumbnails thumbnails = new Thumbnails(path))
+                {
+                    thumbnails.maximum = new Dimension<int> { Width = imageProperties.Dimensions.Width, Height = imageProperties.Dimensions.Height };
+
+                    thumbnails.Create(out bitMap);
+
+                    requestContext.HttpContext.Response.Clear();
+                    requestContext.HttpContext.Response.BufferOutput = true;
+
+                    bitMap.Save(requestContext.HttpContext.Response.OutputStream, imageProperties.ContentType());
+                    requestContext.HttpContext.Response.Flush();
+                    requestContext.HttpContext.Response.Close();
+                }
+            }
+
+            return null;
+        }
+
+        private bool _disposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(true);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                if (bitMap != null)
+                {
+                    bitMap.Dispose();
+                    bitMap = null;
+                }
+                _disposed = true;
+            }
+        }
+    }
+}
