3cd8db6ddfb283f7c991710fc5db4bcbd4f4bd5e
Kirk deDoes <tea@dedogs.com>
2016-09-01 09:26:41 -0400

    Missed files for added logo

---
+++ Models/Helpers/ImageHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.PhotoSamples.Models.Helpers
+{
+    public static class ImageHelper
+    {
+        public static MvcHtmlString Image(this HtmlHelper helper, string src, string altText, int width, int height)
+        {
+            var builder = new TagBuilder("img");
+            builder.MergeAttribute("src", src);
+            builder.MergeAttribute("alt", altText);
+            builder.MergeAttribute("width", Convert.ToString(width));
+            builder.MergeAttribute("height", Convert.ToString(height));
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
